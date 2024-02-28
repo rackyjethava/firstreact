c56e25e3dc49a15ec010d41db85f6a5ea7749588
rackyjethava <raktpx2356@gmail.com>
2024-02-28 19:23:53 +0530

    29_2_24_component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './home';
+import Profil from './profil';
 
 function App() {
   return (
@@ -9,14 +11,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React (home work)
-        </a>
+        </a> */}
+        <Home />
+        <Profil />
       </header>
     </div>
   );
---
+++ src/profil.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function profil(){
+    return <div>
+            <h1>profil component</h1>
+    </div>
+}
+
+export default profil;
