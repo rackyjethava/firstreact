49b28d0903f9bed458babb6ee3fff845c001be40
rackyjethava <raktpx2356@gmail.com>
2024-03-06 12:24:56 +0530

    6_3_24_serch

--- src/user/container/product/Product.js
+++ src/user/container/product/Product.js
@@ -1,33 +1,76 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from 'reactstrap';
 
 function Product(props) {
-
-    const [product, setProduct] = useState([])
+    const [products, setProducts] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
-        getdata()
-    },[])
+        fetchData();
+    }, []);
 
-    const getdata = async () => {
-        const responce = await fetch("https://fakestoreapi.com/products");
-        const data = await responce.json();
-
-        console.log("data");
-        setProduct(data)
-
+    const fetchData = async () => {
+        try {
+            const response = await fetch("https://fakestoreapi.com/products");
+            const data = await response.json();
+            setProducts(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
     }
 
+    const handleSearchChange = (data) => {
+        setSearchQuery(data.target.value);
+    }
+
+    const filteredProducts = products.filter(product =>
+        product.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
     return (
-        <div>
+        <div className='container'>
+            <div className='row'>
+                <div className="input-box">
+                    <input
+                        type="search"
+                        name="search-form"
+                        id="search-form"
+                        className="search-input"
+                        placeholder="Search product"
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                    />
+                </div>
 
-            <div>
-            {
-            product.map((v, i) => (
-                    <h1>{v.title}</h1>
-                ))
-                }
+                {filteredProducts.map((v, i) => (
+                    <div  className='col-3 gy-4'>
+                        <Card style={{ width: '18rem' }} >
+                            <img
+                                height={300}
+                                alt="Sample"
+                                src={v.image}
+                            />
+                            <CardBody>
+                                <CardTitle tag="h5">
+                                    {v.title.substring(0, 20)}...
+                                </CardTitle>
+                                <CardSubtitle
+                                    className="mb-2 text-muted"
+                                    tag="h6"
+                                >
+
+                                </CardSubtitle>
+                                <CardText>
+                                    {v.description.substring(0, 50)}...
+                                </CardText>
+                                <Button>
+                                    add to cart
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </div>
+                ))}
             </div>
-
         </div>
     );
 }
